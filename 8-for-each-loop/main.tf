e78e8bca5b2a65cc43c9b4f14d5e039b0441f008
prasanthbangs2016 <prasanthbangs@gmail.com>
2022-09-25 01:35:26 +0530

    added code

--- 8-for-each-loop/main.tf
+++ 8-for-each-loop/main.tf
@@ -1,6 +1,6 @@
 resource "aws_instance" "web" {
-    #loop from variable loop to create 2 instances
-    for_each = var.sample
+    #loop from variable loop to create 2 instances and converting toset
+    for_each = toset(var.sample)
     ami = "ami-066f46167f3ce8bfe"
     instance_type = "t3.micro"
 
