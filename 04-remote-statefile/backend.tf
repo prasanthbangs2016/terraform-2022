abdd6cb2e4cf1756037bf8ace5c466349afae6f5
prasanthbangs2016 <prasanthbangs@gmail.com>
2022-09-25 00:35:06 +0530

    added code

--- 04-remote-statefile/backend.tf
+++ 04-remote-statefile/backend.tf
@@ -5,3 +5,8 @@ terraform {
     region = "us-east-1"
   }
 }
+
+resource "aws_instance" "web" {
+    ami = "ami-066f46167f3ce8bfe"
+    instance_type = "t3.micro"
+}
--- 04-remote-statefile/instance.tf
+++
@@ -1,6 +0,0 @@
-resource "aws_instance" "web" {
-    ami = "ami-066f46167f3ce8bfe"
-    instance_type = "t3.micro"
-    vpc_security_group_ids = [aws_security_group.web.id]
-  
-}
