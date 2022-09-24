57745d5695242e25560d6574c55cd3df2cd53464
prasanthbangs2016 <prasanthbangs@gmail.com>
2022-09-25 01:54:04 +0530

    added code

--- 7-count-loops/main.tf
+++ 7-count-loops/main.tf
@@ -2,7 +2,13 @@ resource "aws_instance" "web" {
     count = 2
     ami = "ami-066f46167f3ce8bfe"
     instance_type = "t3.micro"
+
+    tags = {
+      #access values from iteration
+      "name" = "sample-${count.index}"
+    }
  
 
   
 }
+
