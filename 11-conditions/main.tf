5d844a6cef5af0bd4555c09f430c5c411875c900
prasanthbangs2016 <prasanthbangs@gmail.com>
2022-09-25 02:27:13 +0530

    added code

---
+++ 11-conditions/main.tf
@@ -0,0 +1,20 @@
+resource "aws_instance" "web" {
+    ami = "ami-066f46167f3ce8bfe"
+    #if any value is not given(default value is null),then pickup t3.micro,if not null pickup 
+    #value given by user
+    instance_type = var.instance_type == null ? "t3.micro" : var.instance_type
+
+    tags = {
+      #access values from iteration
+      Name = "sample-${count.index}"
+    }
+ 
+    
+  
+}
+
+variable "instance_type" {
+    default = null
+  
+}
+
