063f4d6de656a8201e27f81d6cdd159aeb7702ec
prasanthbangs2016 <prasanthbangs@gmail.com>
2022-09-25 01:24:32 +0530

    added code

--- 5-modules-2nd/sharemodule1/test/test.tf
+++
@@ -1,7 +0,0 @@
-resource "aws_instance" "web" {
-    ami = "ami-066f46167f3ce8bfe"
-    instance_type = "t3.micro"
-}
-
-
-  
--- 5-modules-2nd/sharemodule2/backend.tf
+++
@@ -1,7 +0,0 @@
-terraform {
-  backend "s3" {
-    bucket = "1pk"
-    key    = "terraform/modules/terraform.tfstate"
-    region = "us-east-1"
-  }
-}
