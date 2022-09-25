5ef63e4f5ab960218bf072416235452c07665422
prasanthbangs2016 <prasanthbangs@gmail.com>
2022-09-25 14:55:23 +0530

    added code

--- 13-datasource/main.tf
+++ 13-datasource/main.tf
@@ -1,5 +1,8 @@
-data "aws_instances" "test" {}
+data "aws_instances" "test" {
+    instance_state_names = ["running", "stopped"]
+}
 
+#display instances which are running
 output "instances" {
     value = data.aws_instances.test
   
