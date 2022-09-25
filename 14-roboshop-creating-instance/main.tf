da715fc1ea9e30eb1176fdf23730d22db35757f8
prasanthbangs2016 <prasanthbangs@gmail.com>
2022-09-25 17:30:34 +0530

    added code

--- 14-roboshop-creating-instance/main.tf
+++ 14-roboshop-creating-instance/main.tf
@@ -31,7 +31,7 @@ resource "null_resource" "ansible_apply" {
 
     }
     inline = [
-      "ansible-pull -i localhost , -U https://github.com/prasanthbangs2016/roboshop-mutable-ansible roboshop.yml  -e HOSTS=${var.components[${count.index}]}" -e APP_COMPONENT_ROLE=${var.components[${count.index}]}" -e ENV=dev -e MYSQL_PASSWORD=Roboshop@1 -e RABBITMQ_PASSWORD=roboshop123
+      "ansible-pull -i localhost , -U https://github.com/prasanthbangs2016/roboshop-mutable-ansible roboshop.yml  -e HOSTS=${var.components[count.index]} -e APP_COMPONENT_ROLE=${var.components[count.index]} -e ENV=dev -e MYSQL_PASSWORD=Roboshop@1 -e RABBITMQ_PASSWORD=roboshop123
 
     ]
 
